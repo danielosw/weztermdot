c62f4489734570f80d8e3472cec985f50b0325bf
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-27 18:22:46 -0500

    ah

--- wezterm.lua
+++ wezterm.lua
@@ -23,6 +23,43 @@ if Iswindows() then
 else
     config.max_fps = 75
 end
+function getdefualt()
+	if Iswindows() then
+	    return "pwsh"
+	else
+	    return "/usr/bin/fish"
+	end
+end
+Defualt = getdefualt()
+config.launch_menu = {
+  {
+    -- Optional label to show in the launcher. If omitted, a label
+    -- is derived from the `args`
+    label = 'Fish',
+    -- The argument array to spawn.  If omitted the default program
+    -- will be used as described in the documentation above
+    args = { '/usr/bin/fish' },
+
+    -- You can specify an alternative current working directory;
+    -- if you don't specify one then a default based on the OSC 7
+    -- escape sequence will be used (see the Shell Integration
+    -- docs), falling back to the home directory.
+    -- cwd = "/some/path"
+
+    -- You can override environment variables just for this command
+    -- by setting this here.  It has the same semantics as the main
+    -- set_environment_variables configuration option described above
+    -- set_environment_variables = { FOO = "bar" },
+  },
+  {label = 'Powershell',
+  args =  {'pwsh'},
+  },
+  {
+    label = 'Cycle server',
+    args = {Defualt, '-c ssh d932v894@cycle3.eecs.ku.edu'}
+  }
+
+}
 config.term = "wezterm"
 
 -- and finally, return the configuration to wezterm
