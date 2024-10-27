928450d08877daa0f14d641d91ebf8b3b70a34e8
danielosw <65432065+danielosw@users.noreply.github.com>
2024-10-27 18:05:03 -0500

    prepare to use on windows

--- wezterm.lua
+++ wezterm.lua
@@ -3,12 +3,27 @@ local wezterm = require 'wezterm'
 
 -- This will hold the configuration.
 local config = wezterm.config_builder()
-
-config.default_prog = {'/usr/bin/fish'}
+function Iswindows()
+	if package.config:sub(1, 1) == "\\" then
+		return true
+	else
+		return false
+	end
+end
+if Iswindows() then
+    config.default_prog = {'pwsh'}
+else
+    config.default_prog = {'/usr/bin/fish'}
+end
 config.color_scheme = 'Dracula (Official)'
 config.font = wezterm.font("CaskaydiaCove Nerd Font")
-config.enable_tab_bar = false
-config.max_fps = 75
+config.enable_tab_bar = true
+if Iswindows() then
+    config.max_fps = 60
+else
+    config.max_fps = 75
+end
 config.term = "wezterm"
+
 -- and finally, return the configuration to wezterm
 return config
